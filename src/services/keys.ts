0ac844bbd5fb03616dca72eeeae9d3e58a7d20b8
saibot777 <stefantrajkovic777@gmail.com>
2022-10-11 21:40:24 +0200

    Add create item query

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -6,3 +6,11 @@ export const pageCacheKey = (id: string) => `pagecache#${id}`
 
 // Users
 export const usersKey = (userId: string) => `users:#${userId}`;
+
+// Items
+export const itemsKey = (itemId: string) => `items:#${itemId}`;
+export const bidHistoryKey = (itemId: string) => `items:history:#${itemId}`;
+export const itemsByBidsKey = () => 'items:bids';
+export const itemsByViewsKey = () => 'items:views';
+export const itemsByPriceKey = () => 'items:price';
+export const itemsByEndingAtKey = () => 'items:endingAt';
