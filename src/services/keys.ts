da1b871e9c3eb00eb3eca9b5b901072bb934b2c0
saibot777 <stefantrajkovic777@gmail.com>
2022-10-11 18:39:06 +0200

    Add create user query

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -1,1 +1,4 @@
 export const pageCacheKey = (id: string) => `pagecache#${id}`
+
+// Users
+export const usersKey = (userId: string) => `users:#${userId}`;
--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,8 +1,16 @@
+import { client } from '$services/redis';
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
+import {usersKey} from '$services/keys'
 
 export const getUserByUsername = async (username: string) => {};
 
 export const getUserById = async (id: string) => {};
 
-export const createUser = async (attrs: CreateUserAttrs) => {};
+export const createUser = async (attrs: CreateUserAttrs) => {
+    const id = genId()
+    await client.hSet(usersKey(id), {
+        username: attrs.username,
+        password: attrs.password,
+    })
+};
